f9ba3edaceb327a0ede34e5c02dffcbb53e3db3a
Kareem <mahmoudelsonbaty777@gmail.com>
2025-01-15 01:17:35 +0200

    implement user login V2
    services.py
    finish the register_user_service and test it
    add login_user_service and test it
    add login_admin_service but needs improvements so it's hashed
    views.py
    add login_user view with json input & output examples and test it
    add login_admin but not yet fully implemented
    add test_sms for twilio send sms with response example and hashed it
    add check_test_sms_otp for twilio for checking otp with response example and hashed it

--- users/services.py
+++ users/services.py
@@ -1,57 +1,107 @@
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import CustomUser, UserPhoneNumbers
+from users.models import CustomUser, UserPhoneNumbers
 from core.base_models import ResultView
 from users.serializers import RegisterSerializer
+from django.contrib.auth import authenticate
+from users.utils import generate_jwt_token
+import logging
 # Create your services here.
 
-def register_user(registeration_data):
+
+def register_user_service(registration_data):
     result = ResultView()
     try:
-        full_name = registeration_data.get("full_name")
-        first_name=f'{full_name.split(' ')[0] if full_name.__contains__(' ') else full_name}'
-        last_name = ' '.join(full_name.split(' ')[1:])
-        phone_number = registeration_data.get("phone_number")
-        password = registeration_data.get("password")
-        if UserPhoneNumbers.objects.filter(phone_number=phone_number).exists() or CustomUser.objects.filter(username=phone_number).exists():
+        full_name = registration_data.get("full_name")
+        phone_number = registration_data.get("phone_number")
+        password = registration_data.get("password")
+        confirm_password = registration_data.get("confirm_password")
+        if not password or password != confirm_password:
+            result.msg = f"{'Passwords Don\'t match' if password else 'Password is required'}"
+        elif UserPhoneNumbers.objects.filter(phone_number=phone_number).exists() or CustomUser.objects.filter(username=phone_number).exists():
             result.msg = 'A user with this phone number already exists, please login'
         else:
-            serialized_new_user_data = RegisterSerializer(data=registeration_data)
+            serialized_new_user_data = RegisterSerializer(data=registration_data)
             if serialized_new_user_data.is_valid():
-                # new_user = CustomUser.objects.create_user(username=phone_number, password=password, first_name=first_name, last_name=last_name)
-                test: CustomUser = {'id': 1, 'username': phone_number, 'password': password, 'first_name':first_name, 'last_name':last_name}
-                tt = UserPhoneNumbers.objects.create(phone_number=phone_number, is_main_number=True, user = test)#.save()
-                print(tt)
-                # print(new_user, new_user.id)
-                # CustomUser.objects.create_user(username=email.split('@')[0], password=password, email=email, **extra_fields)
+                new_user = CustomUser.objects.create_user(
+                    username=phone_number,
+                    password=password,
+                    first_name=full_name
+                )
+                UserPhoneNumbers.objects.create(
+                    phone_number=phone_number,
+                    is_main_number=True,
+                    user=new_user,
+                    created_by=new_user
+                )
+                result.data = new_user.to_dict()
+                result.msg = f"User {new_user.first_name} created successfully"
                 result.is_success = True
-                # result.data = new_user
-                # user_display_name = f"{new_user.first_name} {new_user.last_name}" if new_user.first_name else new_user.usernamr
-                # result.msg = f"User {user_display_name} created successfully"
             else:
-                result.msg = f'Error happened while serializing new user data.\nError Message: {serialized_new_user_data.errors}'
+                result.msg = f'Error happened while serializing new user data'
                 result.data = {'errors': serialized_new_user_data.errors, 'error messages': serialized_new_user_data.error_messages}
     except Exception as e:
-        result.msg = f'Unexpected Error happened while registering new user. | Error Message: {e}'
+        result.msg = f'Unexpected error happened while registering new user'
         result.data = {'error': str(e)}
     finally:
         return result
 
-def login_user(username, password):
+
+def login_user_service(login_data):
     result = ResultView()
+    logger = logging.getLogger(__name__)
     try:
-        if username and password:
-            user = CustomUser.objects.get(username=username)
-            if user.check_password(password):
-                result.is_success = True
-                result.data = user
-                result.msg = f'User {user.username} logged in successfully'
-            else:
-                result.msg = 'Password is incorrect'
+        username = login_data.get('username', '').strip() # what the empty quotes do ?
+        password = login_data.get('password', '').strip()
+        if not username or not password:
+            result.msg = 'Username is required' if password else 'Password is required' if username  else 'Username & Password are required'
         else:
-            result.msg = 'Username & Password are required' if not (username or password) else 'Username is required' if password else 'Password is required'
+            user_to_auth = authenticate(username=username, password=password) # here django doesn't just check for the password but also check for the is_active field in the db, so no need to check on is_active
+            if user_to_auth is None:
+                logger.warning(f"Failed login attempt for username {username}.")
+                result.msg = 'Invalid Credentials'
+            elif not user_to_auth.is_active:
+                result.msg = 'Account is not active. Please contact support'
+            else:
+                logger.info(f"User {user_to_auth.username} logged in successfully.")
+                token = generate_jwt_token(user_to_auth)
+                result.msg = 'Login Successful'
+                result.data = {
+                    'token': token,
+                    'user': {
+                        'id': user_to_auth.id,
+                        'username': user_to_auth.username,
+                        'full_name': user_to_auth.first_name
+                    }
+                }
+                result.is_success = True
     except Exception as e:
-        result.msg = f'Error happened while logging user {username} in.\n Error Message: {e}'
-        result.data = {'error': e}
+        result.msg = f'Unexpected error happened while logging in user {username}.'
+        result.data = {'error': str(e)}
     finally:
         return result
-    
+
+
+# def login_admin_service(username, password):
+#     result = ResultView()
+#     try:
+#         print('in try')
+#         if username and password:
+#             print('there is username and password')
+#             user = CustomUser.objects.get(username=username)
+#             print('user found and it\'s', user, sep='=>')
+#             if user.check_password(password):
+#                 print('password is correct')
+#                 result.data = user
+#                 result.msg = f'User {user.username} logged in successfully'
+#                 result.is_success = True
+#             else:
+#                 print('password is not correct')
+#                 result.msg = 'Invalid Credentials'
+#         else:
+#             print('either username or password is missing')
+#             result.msg = 'Username & Password are required' if not (username or password) else 'Username is required' if password else 'Password is required'
+#     except Exception as e:
+#         result.msg = f'Unexpected error happened while logging user {username} in.'
+#         result.data = {'error': str(e)}
+#     finally:
+#         return result
--- users/views.py
+++ users/views.py
@@ -1,46 +1,138 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .services import register_user
+from users.services import register_user_service, login_user_service#, login_admin_service
 # Create your views here.
 
 
 '''
-{
-    "username": "testuser",
-    "password": "securepassword",
-    "email": "test@example.com",
-    "first_name": "John",
-    "last_name": "Doe",
-    "image_url": "http://example.com/image.jpg",
-    "country": "USA",
-    "city": "New York",
-    "address": "123 Main St",
-    "postal_code": "10001"
-}
-{
-    "phone_number": "testuser",
-    "password": "securepassword",
-    "confirm_password": "securepass",
-    "full_name": "John han"
-}
+input
 {
     "phone_number": "01555807172",
     "password": "securepassword",
     "confirm_password": "securepassword",
     "full_name": "John han"
 }
+output
+{
+    "is_success": true,
+    "data": "User Data => ID: 1 | Full Name: John han | Main Phone: 01555807172"
+    },
+    "msg": "User John han created successfully"
+}
 '''
+
 @api_view(['POST'])
 def register(request):
-    if request.data.get('password') and request.data.get('password') == request.data.get('confirm_password'):
-        registeration_result = register_user(request.data)
-        print(registeration_result)
-        if registeration_result.is_success:
-            return_status = status.HTTP_201_CREATED
-        else:
-            return_status = status.HTTP_500_INTERNAL_SERVER_ERROR if registeration_result.msg.__contains__('Unexpected') else status.HTTP_400_BAD_REQUEST
-    else:
-        return_status = status.HTTP_400_BAD_REQUEST
-        msg = f"{'Password is required' if not request.data.get('password') else 'Passwords Don\'t match'}"
-    return Response(registeration_result.to_dict(), status=return_status)
+    registration_result = register_user_service(request.data)
+    status_code = (
+        status.HTTP_201_CREATED if registration_result.is_success
+        else status.HTTP_400_BAD_REQUEST
+    )
+    return Response(registration_result.to_dict(), status=status_code)
+
+'''
+input
+{
+    "username": "01555807172",
+    "password": "securepassword"
+}
+output
+{
+    "is_success": true,
+    "data": {
+        "token": "generated token",
+        "user": {
+            "id": 1,
+            "username": "01555807172",
+            "full_name": "John han"
+        }
+    },
+    "msg": "Login Successful"
+}
+'''
+@api_view(['POST'])
+def login_user(request):
+    login_result = login_user_service(request.data)
+    status_code = (
+        status.HTTP_202_ACCEPTED if login_result.is_success
+        else status.HTTP_400_BAD_REQUEST
+    )
+    return Response(login_result.to_dict(), status=status_code)
+
+# @api_view(['POST'])
+# def login_admin(request):
+#     login_result = login_admin_service(request.data.get('username'), request.data.get('password'))
+#     status_code = (
+#         status.HTTP_202_ACCEPTED if login_result.is_success
+#         else status.HTTP_403_FORBIDDEN if login_result.msg.__contains__('not active')
+#         else status.HTTP_400_BAD_REQUEST
+#     )
+#     return Response(login_result.to_dict(), status=status_code)
+
+'''
+pip install twilio => all 15 following packages
+aiohappyeyeballs-2.4.4     ||     aiohttp-3.11.11
+aiohttp-retry-2.8.3        ||     aiosignal-1.3.2
+attrs-24.3.0               ||     certifi-2024.12.14
+charset-normalizer-3.4.1   ||     frozenlist-1.5.0
+propcache-0.2.1            ||     requests-2.32.3
+twilio-9.4.3               ||     urllib3-2.3.0
+idna-3.10     ||    multidict-6.1.0    ||   yarl-1.18.3
+'''
+# from twilio.rest import Client
+# @api_view(['GET'])
+# def test_sms(request):
+#     account_sid = 'get from .env'
+#     auth_token = 'get from .env'
+#     client = Client(account_sid, auth_token)
+#     verification = client.verify.v2.services('VA853ed765c0c235b050c6332aae0f1e41').verifications.create(to='+201118069749', channel='sms')
+#     print(verification.status, verification.valid, verification.sna, verification.to, verification.sid, sep='\n=>')
+'''
+{
+  "sid": "VEaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "service_sid": "VAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "account_sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "to": "+201118069749",
+  "channel": "sms",
+  "status": "pending",
+  "valid": false,
+  "date_created": "2015-07-30T20:00:00Z",
+  "date_updated": "2015-07-30T20:00:00Z",
+  "lookup": {},
+  "amount": null,
+  "payee": null,
+  "send_code_attempts": [
+    {
+      "time": "2015-07-30T20:00:00Z",
+      "channel": "SMS",
+      "attempt_sid": "VLaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    }
+  ],
+  "sna": null,
+  "url": "http s://verify.twilio.com/v2/Services/VAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Verifications/VEaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+}
+'''
+# def check_test_sms_otp(request):#check the otp
+#     account_sid = 'get from .env'
+#     auth_token = 'get from .env'
+#     client = Client(account_sid, auth_token)
+#     verification_check = client.verify.v2.services('VA853ed765c0c235b050c6332aae0f1e41').verification_checks.create(to='+201118069749', code=request.data.get('code'))
+#     print(verification_check.status, verification_check.valid, verification_check.to, verification_check.sid, sep='\n=>')
+'''
+output should be
+{
+  "sid": "VEaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "service_sid": "VAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "account_sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "to": "+201118069749",
+  "channel": "sms",
+  "status": "approved",
+  "valid": true,
+  "amount": null,
+  "payee": null,
+  "sna_attempts_error_codes": [],
+  "date_created": "2015-07-30T20:00:00Z",
+  "date_updated": "2015-07-30T20:00:00Z"
+}
+'''
